a337cb710f3cbe209d02a60c58c32204c71cbf94
hqdem <ilyadem4@gmail.com>
2023-05-13 23:10:46 +0300

    add user model and alembic migrations

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         if isinstance(v, str):
             return v
         return PostgresDsn.build(
-            scheme="postgresql",
+            scheme="postgresql+psycopg2",
             user=values.get("POSTGRES_USER"),
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_HOST"),
---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from app.db.base import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String, nullable=False, index=True)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+    nickname = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
