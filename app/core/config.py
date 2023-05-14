9cae1f367565f2ae2c3bc87298be0312a594d025
hqdem <ilyadem4@gmail.com>
2023-05-14 21:05:47 +0300

    fix settings variables names

--- app/core/config.py
+++ app/core/config.py
@@ -4,15 +4,15 @@ from pydantic import BaseSettings, validator, PostgresDsn
 
 
 class Settings(BaseSettings):
-    API_PREFIX: str = 'api/v1'
+    API_PREFIX: str = '/api/v1'
     SECRET_KEY: str
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
 
-    POSTGRES_HOST: str
-    POSTGRES_USER: str
+    DB_HOST: str
+    DB_USER: str
     POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    DB_NAME: str
     SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
 
     @validator("SQLALCHEMY_DATABASE_URI", pre=True)
@@ -21,10 +21,10 @@ class Settings(BaseSettings):
             return v
         return PostgresDsn.build(
             scheme="postgresql+psycopg2",
-            user=values.get("POSTGRES_USER"),
+            user=values.get("DB_USER"),
             password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_HOST"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
+            host=values.get("DB_HOST"),
+            path=f"/{values.get('DB_NAME') or ''}",
         )
 
     class Config:
