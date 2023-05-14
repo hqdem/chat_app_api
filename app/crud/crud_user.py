e29a966f01088d7c80b5e1b6a48d6e150bba3685
hqdem <ilyadem4@gmail.com>
2023-05-14 20:41:42 +0300

    add crud for user

---
+++ app/crud/crud_user.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from app.core.security import get_password_hash
+from app.crud.base import CRUDBase
+from app.schemas.user import UserCreate, UserUpdate
+from app.models.user import User
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+    def get_user_by_login(self, db: Session, *, login: str) -> User | None:
+        return db.query(User).filter(User.login == login).first()
+
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        obj_in_dict = obj_in.dict(exclude={'is_superuser', 'is_active'})
+        plain_password = obj_in_dict.pop('password')
+
+        db_obj = User(**obj_in_dict, hashed_password=get_password_hash(plain_password))
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
